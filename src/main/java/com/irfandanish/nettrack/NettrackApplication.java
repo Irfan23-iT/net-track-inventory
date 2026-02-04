d51ccf1c5cacf0d458f7a2aabc60bd9aa8ef60a3
Irfan Danish <mohamadirfandanish23@gmail.com>
2026-02-04 20:43:39 +0800

    Initialize Spring Boot project structure

---
+++ src/main/java/com/irfandanish/nettrack/NettrackApplication.java
@@ -0,0 +1,13 @@
+package com.irfandanish.nettrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irfandanish/nettrack/NettrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irfandanish.nettrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
