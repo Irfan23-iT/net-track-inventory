fd21619348e89730da26bb8f2f27d782c8e3c080
Irfan Danish <mohamadirfandanish23@gmail.com>
2026-02-04 22:01:32 +0800

    Migrate database from H2 to MySQL for permanent storage

--- src/main/java/com/irfandanish/nettrack/NettrackApplication.java
+++ src/main/java/com/irfandanish/nettrack/NettrackApplication.java
@@ -12,16 +12,5 @@ public class NettrackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NettrackApplication.class, args);
-    }
-
-    // This method runs automatically on startup!
-    @Bean
-    public CommandLineRunner demo(AssetRepository repository) {
-        return (args) -> {
-            // Save a couple of assets
-            repository.save(new Asset("Cisco Catalyst 2960", "C-2960-001", "Switch", "Active"));
-            repository.save(new Asset("Asus TUF F15", "TUF-F15-999", "Laptop", "Assigned"));
-            repository.save(new Asset("Cisco ISR 4000", "ISR-4000-X", "Router", "Maintenance"));
-        };
-    }
+    } 
 }
